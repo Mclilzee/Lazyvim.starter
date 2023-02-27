9efd8a31d47133644b8c3889f51b97131103ff12
Emad Ali <mc.lilzee@hotmail.com>
2023-02-27 11:52:47 +0100

    Seperating setting default color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,8 @@
 return {
   {
-    "EdenEast/nightfox.nvim",
-  },
-
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "carbonfox",
-      transparent = true,
+      colorscheme = "tokyonight",
     },
   },
 }
