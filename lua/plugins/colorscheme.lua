13edcb09205e42d54f6458912317e1368d055fc8
Emad Ali <mc.lilzee@hotmail.com>
2023-04-05 01:10:35 +0200

    Fix gruvbox italic settings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,12 @@ return {
     "ellisonleao/gruvbox.nvim",
     opts = {
       transparent_mode = true,
-      italic = false,
+      italic = {
+        strings = false,
+        comments = false,
+        operators = false,
+        folds = false,
+      },
       palette_overrides = {
         -- bright_blue = "#fffbac", -- variable names
         -- bright_red = "#cc241d", -- keywords
